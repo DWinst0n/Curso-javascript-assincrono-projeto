b039bfa5da5eef86e69bd7a3fcf71875e43cb65a
DWinst0n <daviwinstonsm@gmail.com>
2025-01-11 16:57:08 -0300

    implementação de checkbox customizado na página de login

--- Javascript/login.js
+++ Javascript/login.js
@@ -2,3 +2,12 @@ window.addEventListener('load', () => {
   const containerPrincipal = document.querySelector('.container__principal');
   containerPrincipal.classList.add('loaded');
 });
+const checkbox = document.getElementById("checkbox");
+const checkboxContainer = document.getElementById("checkbox-customizado");
+
+checkbox.addEventListener('click', () => {
+  checkboxContainer.classList.add('checked');
+  if (checkbox.checked) {
+    checkboxContainer.classList.remove('checked');
+  }
+})
